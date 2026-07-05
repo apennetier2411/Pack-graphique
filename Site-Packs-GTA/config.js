f526a04f4573bac67d94641c9a0b6540a21ffc95
apennetier2411 <apennetier11@gmail.com>
2026-07-05 19:13:08 +0200

    Update config.js

--- Site-Packs-GTA/config.js
+++ Site-Packs-GTA/config.js
@@ -4,5 +4,5 @@ const CONFIG = {
     
     // Récupère ces infos dans Supabase -> Project Settings -> API
     SUPABASE_URL: "https://lbidqhqyygcjnejritnj.supabase.co",
-    SUPABASE_ANON_KEY: "sb_publishable_2oga_MqscX7eqT36iGgP7A_SPIvwEHV"
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiaWRxaHF5eWdjam5lanJpdG5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNjM5OTIsImV4cCI6MjA5ODgzOTk5Mn0.5nYjj0ZaFygHRMA8LGNRnEAqMFsz4_No0cpJIRzzR_U"
 };
