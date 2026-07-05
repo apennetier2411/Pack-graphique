40ade5fec30c1502e5d0aa5588437bbc456f29e8
apennetier2411 <apennetier11@gmail.com>
2026-07-05 18:37:49 +0200

    Add files via upload

---
+++ Site-Packs-GTA/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    ADMIN_USER: "admin",
+    ADMIN_PASS: "ayfox2025",
+    
+    // Récupère ces infos dans Supabase -> Project Settings -> API
+    SUPABASE_URL: "https://lbidqhqyygcjnejritnj.supabase.co",
+    SUPABASE_ANON_KEY: "sb_publishable_2oga_MqscX7eqT36iGgP7A_SPIvwEHV"
+};
